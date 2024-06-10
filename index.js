7df52f63c35185ab54b1dcd277f014c472a014a5
Vitor Inzize Rausis <113448819+vitorrausis@users.noreply.github.com>
2024-06-10 20:04:14 -0300

    ot14

--- index.js
+++ index.js
@@ -1,61 +1,33 @@
-const express = require ("express");
+import express from 'express';  
+import { buscarUfs ,buscarUfPorId ,buscarUfsPorNome } from './servicos/servicos.js';
+
 const app = express();
-app.use(express.json());
 
+/*const buscarUfsPorNome=(nomeUf)=>{
+    return colecaoUf.filter(uf=>uf.nome.toLowerCase().includes(nomeUf.toLowerCase()))
+};*/
 
-app.listen(3080,()=>{
-    console.log("Servidor Rodando");
-})
-
-let games = [
-    {title:"Elden Ring", studio: "FromSoftware", price:60},
-    {title:"Persona 5 Royal", studio: "Atlus", price:300},
-    {title:"Call of Duty: Modern Warfare 3", studio: "Infinity Ward", price:350},
-    {title:"Disco Elysium", studio: "ZA/UM", price:50},
-    {title:"Neon White", studio: "Angel Matrix", price:125},
-    {title:"NORCO", studio: "Geography of Robot", price:30},
-    {title:"Stray", studio: "BlueTwelve", price:150},
-    {title:"God of War", studio: "Santa Monica Studios", price:250},
-    {title:"Ghostwire Tokyo", studio: "Tango Gameworks", price:300},
-    {title:"F1 23", studio: "Codemasters", price:350}
-
-
-]
-
-app.get("/",(req,res)=>{
-    res.json(games);
-})
-
-app.post("/novogame",(req, res) => {
-    let title = req.body.title;
-    let studio = req.body.studio;
-    let price = req.body.price;
-
-    console.log(title);
-    console.log(studio);
-    console.log(price);
-
-    let newGame = { title, studio, price }
-
-    games.push(newGame);
-
-    res.send("OK");
-
-    app.put('/novogame/:index', (req, res)=>{
-        const{ index } = req.params;
-        let title = req.body.title;
-        let studio = req.body.studio;
-        let price = req.body.price;
-
-        games[index] = {title,  studio, price};
-
-        return res.json(games);
-    })
-
+app.get('/ufs', (req, res) => {
+    const nomeUf = req.query.busca;
+    const resultado = nomeUf ? buscarUfsPorNome(nomeUf): buscarUfs();
+    if(resultado.length > 0){
+        res.json(resultado);
+    }else{
+        res.status(404).send({"erro":"Nenhuma UF encontrada"})
+    }
 });
 
-app.delete("/:index",(req, res) => {
-    const {index} = req.params;
-    games.splice(index,1);
-    return res.json({message: "O jogo foi deletado"});
-})
+app.get('/ufs/:iduf', (req, res) => {
+   const uf = buscarUfPorId(req.params.iduf);
+if(uf){
+    res.json(uf);
+}else if(isNaN(parseInt(req.params.iduf))){
+    res.status(400).send({"erro":"requisição Inválida"});
+}else{
+    res.status(404).send({"erro":"UF não encontrada"});
+}
+});
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado na porta 8080')
+});
